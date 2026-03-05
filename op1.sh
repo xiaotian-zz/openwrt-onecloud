03ac78c1b114b9b936ac38431b602d678d97b510
xiaotian-zz <ppnc@qq.com>
2026-03-05 23:26:56 +0800

    Update op1.sh

--- op1.sh
+++ op1.sh
@@ -16,7 +16,7 @@
 # Add a feed source
 #echo 'src-git passwall_packages https://github.com/lxiaya/openwrt-passwall-packages.git;main' >>feeds.conf.default
 #echo 'src-git homeproxy https://github.com/lxiaya/openwrt-homeproxy.git' >>feeds.conf.default
-#echo 'src-git openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
+echo 'src-git openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
 #echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >>feeds.conf.default
 #echo 'src-git clash https://github.com/lxiaya/onecloud-package' >>feeds.conf.default
